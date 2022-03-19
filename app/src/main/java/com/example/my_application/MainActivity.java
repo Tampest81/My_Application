fb5738a1c3a08471b048b1cbe93db05ffdd9248a
Aluno <aluno@LI.LOCAL>
2022-03-18 22:20:11 -0300

    Tarefa....
    Terminar de passar para a aplicação...

--- app/src/main/java/com/example/my_application/MainActivity.java
+++ app/src/main/java/com/example/my_application/MainActivity.java
@@ -93,7 +93,7 @@ public class MainActivity extends AppCompatActivity
             }
             super.onActivityResult(requestCode, resultCode, data);
         }
-        
+
     public class GPStracker implements LocationListener
     {
         Context context;
