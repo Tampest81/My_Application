28c94402c7e9ff10e432f9a2f471dea13e2dfee4
Aluno <aluno@LI.LOCAL>
2022-03-18 22:31:23 -0300

    finalizado..

--- app/src/main/java/com/example/my_application/MainActivity.java
+++ app/src/main/java/com/example/my_application/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.my_application;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
@@ -13,6 +14,7 @@ import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.location.Location;
 import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
@@ -37,12 +39,13 @@ public class MainActivity extends AppCompatActivity
         buttonGEO = (Button) findViewById(R.id.button3);
         ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},123);
         //
-        buttonGEO.setOnClickListener(new View.OnClickListener(){
+        buttonGEO.setOnClickListener(new View.OnClickListener()
+        {
             //
             @Override
             public void onClick(View view)
             {
-                GPStracker = new GPStracker(getApplication());
+                GPStracker g = new GPStracker(getApplication());
                 Location l= g.getLocation();
 
                 // se a localização não for nula, ela ira mostrar na tela através do comando Toast as variáveis lat e lon.
@@ -66,7 +69,7 @@ public class MainActivity extends AppCompatActivity
         findViewById(R.id.imageView3).setOnClickListener(new View.OnClickListener()
         {
             @Override
-                public void OnClick(View view)
+                public void onClick(View view)
                 {
                     TirarFoto();
                 }
@@ -97,14 +100,46 @@ public class MainActivity extends AppCompatActivity
     public class GPStracker implements LocationListener
     {
         Context context;
-
+        // criação da variável e atribuindo valores à ela
         public GPStracker(Context c)
         {
             context = c;
         }
-        public  Location getLocation()
-        {
-            if(ContextCompat.checkSelfPermission(context, Manifest))
+        // criação do método GetLocation para retornar a localização
+        public Location getLocation(){
+        // verificação se a permissão está ativa, caso esteja desativada, irá printar através do comando Toast, "Não foi permitir"
+            if(ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) !=
+                    PackageManager.PERMISSION_GRANTED){
+        // Mostra uma modal na tela "Não foi permitir" caso o usuário não tenha permitido o uso do fine location
+                Toast.makeText(context, "Não foi permitir", Toast.LENGTH_SHORT).show();
+                return null;
+            }
+            LocationManager lm = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+            boolean isGPSEnabled = lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+        //Caso o gps esteja ativado, ele irá retornar a posição recebida atráves do lastknown location, que receberá a ultima posição providenciada
+        //pelo Location manager gps provider, tudo isso apenas irá funcionar, após o requestLocation fizer a requisição das atualizações da posição
+        //do usuário
+
+            if(isGPSEnabled){
+                lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 6000, 10, this);
+                Location l = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                return l;
+            } else {
+        // Caso não esteja habilitado irá mostrar na tela, pedindo que o usuário habilite o GPS
+                Toast.makeText(context, "Por favor, habitar o GPS!", Toast.LENGTH_LONG).show();
+            }
+        // recebendo o null para satisfazer todas as opções
+            return null;
         }
+        // Métodos para o GPStracker funcionar corretamente, ele pede a existência deles, mas não há a necessidade de ter um valor.
+        @Override
+        public void onProviderDisabled(@NonNull String provider) {    }
+        //
+        @Override
+        public void onLocationChanged(@NonNull Location location) {    }
+        //
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {    }
     }
 }
